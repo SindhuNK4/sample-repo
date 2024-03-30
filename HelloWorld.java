50fcf7e8b6cbdfb893177b0451c0754226a14202
SindhuNK4 <sindhuunk@gmail.com>
2024-03-30 10:22:32 +0530

    Updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
-    System.out.println("Hello, World!! This is for webhook.");
+    System.out.println("Hello, World!! This is for trying webhook.");
   }
 }
